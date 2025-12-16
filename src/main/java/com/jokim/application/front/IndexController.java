f9562b61781f21b3c41a0df7aa2e6e164bdf6b02
jok-MacBook <jokim.dev@gmail.com>
2025-12-16 17:16:15 +0900

    Feat: IndexController 추가

---
+++ src/main/java/com/jokim/application/front/IndexController.java
@@ -0,0 +1,13 @@
+package com.jokim.application.front;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
